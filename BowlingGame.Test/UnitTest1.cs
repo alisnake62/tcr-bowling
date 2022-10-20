44686f00b128eeaa140d8d5bb62401514313781a
clement <clementsergent@yahoo.fr>
2022-10-20 11:43:26 +0200

    Working version

--- BowlingGame.Test/UnitTest1.cs
+++ BowlingGame.Test/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace BowlingGame.Test
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(1, 1, 0.001, "Account not debited correctly");
         }
     }
 }
